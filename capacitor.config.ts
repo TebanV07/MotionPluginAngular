6899e8a5faa3003719beb0526e5e84aaa05f781a
vasqesteban024 <vasqesteban024@gmail.com>
2025-03-26 16:27:50 +0000

    apk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,9 +3,18 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'AR Orientation',
-  webDir: 'dist',
+  webDir: 'dist/angular-motion/browser',
+  android: {
+    buildOptions: {
+      keystorePath: '',
+      keystoreAlias: ''
+    }
+  },
+  ios: {
+    scheme: 'App'
+  },
   plugins: {
-    Motion: {}
+    // Configuración de plugins si los necesitas
   }
 };
 
